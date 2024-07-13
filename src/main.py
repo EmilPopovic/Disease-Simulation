2f57dbd0c7af876ffea711cea3be2271340b5626
EmilPopovic <emil.popovic@pm.me>
2024-07-13 17:35:23 +0200

    kinda works now

--- src/main.py
+++ src/main.py
@@ -1,3 +1,6 @@
+import numpy as np
+import pylab as plt
+
 from src.individual import Individual
 from src.population import Population
 
@@ -6,7 +9,7 @@ Individual.MINIMUM_MATING_AGE = 18
 Individual.MAXIMUM_MATING_AGE = 50
 Individual.OLD_AGE_LIMIT = 70
 Individual.MAXIMUM_AGE = 120
-Individual.INFECTION_KILL_AGE = 40
+Individual.INFECTION_KILL_AGE = 50
 
 Individual.MATING_PROBABILITY = 0.11
 Individual.OLD_AGE_DEATH_PROBABILITY = 0.05
@@ -19,7 +22,7 @@ Population.INITIAL_INFECTIONS = 10
 Population.MINIMUM_INITIAL_AGE = 0
 Population.MAXIMUM_INITIAL_AGE = 120
 Population.MEAN_INITIAL_AGE = 40
-Population.SD_INITIAL_AGE = 20
+Population.SD_INITIAL_AGE = 30
 
 Population.POTENTIAL_PARTNER_COUNT = 30
 Population.MAXIMUM_AGE_DIFFERENCE = 10
@@ -27,10 +30,7 @@ Population.RELATIONSHIP_PROBABILITY = 0.05
 
 Population.MINIMUM_INCEST_DISTANCE = 2
 
-ITERATIONS = 200
-
-
-# todo https://plotly.com/python/network-graphs/
+ITERATIONS = 1000
 
 
 def main():
@@ -42,10 +42,44 @@ def main():
 
     for i in range(ITERATIONS):
         pop.advance_sim()
-        pop.print_yearly_stats(pop.year)
+        #pop.print_yearly_stats(pop.year)
 
     pop.print_stats('Final population stats')
 
+
+    X = np.arange(0, ITERATIONS + 1, 1)
+
+    pop_data = pop.yearly_population_to_ndarray().T
+
+    total_population = pop_data
+
+    plt.subplot(2, 2, 1)
+    plt.stackplot(
+        X,
+        *total_population,
+        baseline='zero',
+        labels=['0-19', '20-39', '40-59', '60-79', '80-99', '100+']
+    )
+    plt.title('Total population by age bin')
+    plt.axis('tight')
+    plt.legend(loc='upper left')
+
+    norm_population = pop_data / pop_data.sum(axis=0, keepdims=True) * 100
+
+    plt.subplot(2, 2, 2)
+    plt.stackplot(
+        X,
+        *norm_population,
+        baseline='zero',
+        labels=['0-19', '20-39', '40-59', '60-79', '80-99', '100+']
+    )
+    plt.title('Normalized population by age bin')
+    plt.axis('tight')
+    plt.ylim(0, 100)
+    plt.legend(loc='upper left')
+
+
+    plt.show()
 
 if __name__ == '__main__':
     main()
